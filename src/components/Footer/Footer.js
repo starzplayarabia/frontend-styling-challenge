2071d90ec69198649e78d015bb80ef1de8e4793d
ivlo <ivan.lopez@bluetab.net>
2019-11-27 19:33:24 +0100

    chore: layout and styles for Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,69 +1,19 @@
 import React from "react";
-import AppLogos from "../AppLogos";
-import {
-  HeadingStyled,
-  LogoStyled
-} from "./styles/Styled";
-
-const LINK_ROLE = "link";
-
-function getLanguageListItems() {
-  return [
-    {
-      code: "en",
-      text: "English",
-      direction: "ltr",
-      separator: ","
-    },
-    {
-      code: "ar",
-      text: "Arabic",
-      direction: "rtl",
-      separator: "،"
-    },
-    {
-      code: "fr",
-      text: "Français",
-      direction: "ltr",
-      separator: ","
-    }
-  ];
-}
+import FooterNav from "../FooterNav";
+import { FooterStyled, LogoStyled, AppLogosStyled } from "./styles/Styled";
 
 function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer>
-      <nav>
-        <HeadingStyled rank={5} text="Questions?" size={"default"} />
-        <ul>
-          <li><a href="" role={LINK_ROLE} title="Contact">
-            Contact
-          </a></li>
-          <li><a href="" role={LINK_ROLE} title="FAQ">
-            FAQ
-          </a></li>
-          <li><a href="" role={LINK_ROLE} title="Terms & Conditions">
-            Terms & Conditions
-          </a></li>
-        </ul>
-      </nav>
-
-      <nav>
-        <HeadingStyled rank={5} text="Language" size={"default"} />
-        <ul>
-          { getLanguageListItems().map(language => (<li><a href="" title={language.text}>{language.text}</a></li>)) }
-        </ul>
-      </nav>
-
-      <AppLogos />
-
+    <FooterStyled>
+      <FooterNav />
+      <AppLogosStyled />
       <div>
         <small>© {currentYear}</small>
         <LogoStyled />
       </div>
-    </footer>
+    </FooterStyled>
   );
 }
 
--- src/components/Footer/styles/Styled.js
+++ src/components/Footer/styles/Styled.js
@@ -1,20 +1,16 @@
 import styled from "styled-components";
 import { Logo } from "../../Logo";
-import { Heading } from "../../Heading";
+import AppLogos from "../../../components/AppLogos";
 import { media } from "../../../styles/media";
 
-export const HeadingStyled = styled(Heading)`
-  margin-bottom: ${props => props.theme.spacing.small};
-  /* Media queries */
-  ${media.medium`
-    display: inline-block;
-    margin-right: ${props => props.theme.spacing.small};
-    ${props => `margin-${props.theme.locale.dir === "rtl" ? "left" : "right"}: ${props.theme.spacing.small}`};
-    margin-bottom: 0;
-    &&& {
-      font-weight: ${props => props.theme.font.weight.light};
-    }
-  `};
+export const FooterStyled = styled.footer`
+  padding: ${props => props.theme.spacing.large} 0;
+  background: ${props => props.theme.color.greyBlack};
+  text-align: center;
+  small {
+    font-size: ${props => props.theme.font.size.large};
+    color: ${props => props.theme.color.white};
+  }
 `;
 
 export const LogoStyled = styled(Logo)`
@@ -26,4 +22,11 @@ export const LogoStyled = styled(Logo)`
     width: 100px;
     height: 14px;
   `};
+`;
+
+export const AppLogosStyled = styled(AppLogos)`
+  margin: 0 0 ${props => props.theme.spacing.large} 0;
+  a {
+    margin: 0 ${props => props.theme.spacing.small};
+  }
 `;
